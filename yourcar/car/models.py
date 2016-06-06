60cad298071457fbe49d825b80b14c2a6b077c34
Italo Paiva <italosensei@hotmail.com>
2016-06-05 23:50:54 -0300

    Creating models

--- yourcar/car/models.py
+++ yourcar/car/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.core import validators
 
-# Create your models here.
+class Car(models.Model):
+    car_model = models.CharField(max_length=20)
+    color = models.CharField(max_length=20)
+    year = models.SmallIntegerField(
+        help_text=_('Use year as YYYY.'),
+        validators=[validators.RegexValidator(
+            r'^[0-9]{4}$',
+            _('Year in invalid format!'),
+            'invalid'
+        )]
+    )
+    mileage = models.IntegerField(
+        default=0,
+        validators=[validators.MinValueValidator(0)],
+        help_text=_("Or your car is brand new or it have some mileage traveled")
+    )
+
+    def __str__(self):
+        return self.car_model + "/" + self.year
+
+class OilChange(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    date = models.DateTimeField('date changed')
+    mileage = models.IntegerField(
+        default=0,
+        validators=[validators.MinValueValidator(0)]
+    )
+
+class Refuel(models.Model):
+    REGULAR_GAS = _("Regular gas")
+    FUEL_TYPES = (
+        (REGULAR_GAS, _("Regular gas")),
+        (_("Premium gas"), _("Premium gas")),
+        (_("Alcohol"), _("Alcohol")),
+        (_("Diesel"), _("Diesel")),
+    )
+
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    date = models.DateTimeField('date refueled')
+    liters = models.DecimalField(max_digits=7, decimal_places=3)
+    fuel_price = models.DecimalField(max_digits=4, decimal_places=2)
+    mileage = models.IntegerField(
+        default=0,
+        validators=[validators.MinValueValidator(0)]
+    )
+    fuel_type = models.CharField(
+        max_length=20,
+        choices=FUEL_TYPES,
+        default=REGULAR_GAS
+    )
